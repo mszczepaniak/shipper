ade4c76bf11672db88ad5575f39bffe2ad561b1a
mszczepaniak <mr.szczepaniak@gmail.com>
2024-08-05 07:56:00 +0200

    Models

---
+++ src/app/models/route-model.ts
@@ -0,0 +1,14 @@
+export interface RouteData {
+  routeId: string;
+  fromPort: string;
+  toPort: string;
+  legDuration: string;
+  points: RoutePoint[];
+}
+
+export interface RoutePoint {
+  longitude: number;
+  latitude: number;
+  timestamp: number;
+  speed: number | null;
+}
