18d6a1ad0b5ead528a4c35541092e62d5a38403c
mszczepaniak <mr.szczepaniak@gmail.com>
2024-08-05 07:55:11 +0200

    Route selector

---
+++ src/app/components/route-selector/route-selector.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { RouteDataService } from '../../services/route-data/route-data.service';
+import { ShipperStateService } from '../../services/shipper-state/shipper-state.service';
+import { RouteData } from '../../models/route-model';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-route-selector',
+  templateUrl: './route-selector.component.html',
+  styleUrls: ['./route-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RouteSelectorComponent implements OnInit, OnDestroy {
+  routes: RouteData[] = [];
+  selectedRoute: RouteData | null = null;
+
+  private routesSubscription: Subscription;
+
+  constructor(
+    private routeDataService: RouteDataService,
+    private shipperStateService: ShipperStateService,
+  ) {}
+
+  ngOnInit() {
+    this.routesSubscription  = this.routeDataService.loadRoutes().subscribe((routes: RouteData[]) => {
+      this.routes = routes;
+    });
+  }
+
+  onRouteSelect(route: RouteData) {
+    this.selectedRoute = route;
+    this.shipperStateService.setSelectedRoute(route);
+  }
+
+  resetRoute() {
+    this.selectedRoute = null;
+    this.shipperStateService.setSelectedRoute(null);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routesSubscription) {
+      this.routesSubscription.unsubscribe();
+    }
+  }
+}
